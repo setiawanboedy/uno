328fa59c1f33feb2730dcf5bd13788150d19ee7b
jasaapplab <137791896+jasaapplab@users.noreply.github.com>
2023-07-26 11:52:14 +0800

    final

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ from fastapi.encoders import jsonable_encoder
 import matplotlib.pyplot as plt
 # import os
 import uvicorn
-import base64
 
 # data science
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
-import seaborn as sns
 
 # signal processing
 from scipy import signal
@@ -24,6 +21,8 @@ from scipy.integrate import trapz
 app = FastAPI()
 file_path = "./data/heart.csv"
 app.mount('/data', StaticFiles(directory="data"), name="data")
+# Mount the "images" folder to be served as static files
+app.mount("/images", StaticFiles(directory="images"), name="images")
 
 @app.get('/')
 async def home():
@@ -64,8 +63,13 @@ async def original_signal():
 async def deteksi_r():
     ecg = setup(file_path)
     
+    sampfrom = 0
+    sampto = 3000
+    cond_slice = (ecg.index >= sampfrom) & (ecg.index < sampto)
+    ecg_slice = ecg.hart[cond_slice]
+    
     # deteksi puncak
-    peaks, similarity, ecg_slice = peaks_similarity(ecg)
+    peaks, similarity = peaks_similarity(ecg_slice)
     
     image = './images/deteksi.jpg'
     
@@ -92,15 +96,19 @@ async def deteksi_r():
     plt.savefig(image)
     plt.close()
     
-    with open(image, "rb") as image_file:
-        encode_image = base64.b64encode(image_file.read())
-    return encode_image
+    response = {
+    'image': 'http://34.101.125.197/images/deteksi.jpg',
+    }
+    
+    encoder = jsonable_encoder(response)
+    
+    return JSONResponse(content= encoder)
 
 @app.get("/koreksi-interval-r")
 async def koreksi_interval_r():
     image = './images/koreksi.jpg'
     ecg = setup(file_path)
-    peaks, _, _ = peaks_similarity(ecg)
+    peaks, _ = peaks_similarity(ecg.hart)
     rr, rr_corrected = group_rr_corrected(peaks)
     
     plt.figure(figsize=(20, 5))
@@ -116,25 +124,34 @@ async def koreksi_interval_r():
     plt.savefig(image)
     plt.close()
      
-    with open(image, "rb") as image_file:
-        encode_image = base64.b64encode(image_file.read())
-    return encode_image
+    response = {
+    'image': 'http://34.101.125.197/images/koreksi.jpg',
+    }
+    
+    encoder = jsonable_encoder(response)
+    
+    return JSONResponse(content= encoder)
 
 @app.get("/timedomain")
 async def timedomain():
 
     ecg = setup(file_path)
-    peaks, _, _ = peaks_similarity(ecg)
+    peaks, _ = peaks_similarity(ecg.hart)
     _, rr_corrected = group_rr_corrected(peaks)
     
+    ibi = "{:.2f}".format(np.mean(rr_corrected))
+    sdnn ="{:.2f}".format(np.std(rr_corrected))
+    rmssd ="{:.2f}".format(np.sqrt(np.mean(np.square(np.diff(rr_corrected)))))
+    bpm ="{:.0f}".format(60000/np.mean(rr_corrected))
+        
     response = {
-        'ibi': np.mean(rr_corrected),
-        'sdnn': np.std(rr_corrected),
-        'rmssd': np.sqrt(np.mean(np.square(np.diff(rr_corrected)))),
-        'bpm': 60000/np.mean(rr_corrected)
+        'ibi': float(ibi),
+        'sdnn': float(sdnn),
+        'rmssd': float(rmssd),
+        'bpm': float(bpm)
     }
     
-    encoder = jsonable_encoder({'data': response})
+    encoder = jsonable_encoder(response)
     
     return JSONResponse(content= encoder)
 
@@ -142,12 +159,24 @@ async def timedomain():
 async def frequencydomain():
 
     ecg = setup(file_path)
-    peaks, _, _ = peaks_similarity(ecg)
+    peaks, _ = peaks_similarity(ecg.hart)
     _, rr_corrected = group_rr_corrected(peaks)
     
 
     # create interpolation function based on the rr-samples.
-    fxx,pxx = fxx_pxx_interpolated(rr_corrected)
+    fs = 4.0
+    steps = 1 / fs
+
+    # create interpolation function based on the rr-samples.
+    x = np.cumsum(rr_corrected) / 1000.0
+    f = interp1d(x, rr_corrected, kind='cubic')
+
+    # now we can sample from interpolation function
+    xx = np.arange(1, np.max(x), steps)
+    rr_interpolated = f(xx)
+    
+        # Estimate the spectral density using Welch's method
+    fxx, pxx = signal.welch(x=rr_interpolated, fs=fs, window='flattop')
 
     '''
     Segement found frequencies in the bands
@@ -163,22 +192,27 @@ async def frequencydomain():
     vlf = trapz(pxx[cond_vlf], fxx[cond_vlf])
     lf = trapz(pxx[cond_lf], fxx[cond_lf])
     hf = trapz(pxx[cond_hf], fxx[cond_hf])
+    
+    vlf_formated = "{:.2f}".format(vlf)
+    lf_formated = "{:.2f}".format(lf)
+    hf_formated = "{:.2f}".format(hf)
 
     response = {
-        'vlf': vlf,
-        'lf': lf,
-        'hf': hf
+        'vlf': float(vlf_formated),
+        'lf': float(lf_formated),
+        'hf': float(hf_formated)
     }
     
     encoder = jsonable_encoder({'data': response})
     
     return JSONResponse(content= encoder)
+    # return {"succeess"}
 
 @app.get("/power-spektral")
 async def power_spektral():
     image = './images/spektral.jpg'
     ecg = setup(file_path)
-    peaks, _, _ = peaks_similarity(ecg)
+    peaks, _ = peaks_similarity(ecg.hart)
     _, rr_corrected = group_rr_corrected(peaks)
     
     fxx,pxx = fxx_pxx_interpolated(rr_corrected)
@@ -239,9 +273,13 @@ async def power_spektral():
     plt.savefig(image)
     plt.close()
     
-    with open(image, "rb") as image_file:
-        encode_image = base64.b64encode(image_file.read())
-    return encode_image
+    response = {
+    'image': 'http://34.101.125.197/images/spektral.jpg',
+    }
+    
+    encoder = jsonable_encoder(response)
+    
+    return JSONResponse(content= encoder)
 
 # run server
 
@@ -279,17 +317,13 @@ def fxx_pxx_interpolated(rr_corrected):
 
 def peaks_similarity(df_time_index):
         # set range data
-    sampfrom = 0
-    sampto = 3000
-    
-    cond_slice = (df_time_index.index >= sampfrom) & (df_time_index.index < sampto)
-    ecg_slice = df_time_index.hart[cond_slice]
+
 
     # detect peaks
-    peaks, similarity = detect_peaks(ecg_slice, threshold=0.3)
+    peaks, similarity = detect_peaks(df_time_index, threshold=0.3)
     
    
-    return peaks, similarity, ecg_slice
+    return peaks, similarity
 
 def group_rr_corrected(peaks):
         # group peaks
@@ -311,7 +345,8 @@ def time_to_milliseconds(df):
     # Mengubah waktu menjadi dalam milidetik dimulai dari 0
     df["time"] = (df["time"] - start_time).dt.total_seconds() * 1000
     df["time"] = df["time"].astype(int)
-    return df.set_index('time')
+    df = df.set_index('time')
+    return df
 
 def detect_peaks(ecg_signal, threshold=0.3, qrs_filter=None):
     '''
